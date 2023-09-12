cdd2a081e4e55140fff196443c0a2a7b78071e47
Abdyukov Zufar <swat55551@gmail.com>
2023-09-12 17:44:19 +0300

    Add Show Example MatPlotLib 3D

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits import mplot3d #for 3D
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Single Points
+ax = plt.axes(projection="3d")
+#ax.scatter(3,5,7)
 
+#x_data = np.random.randint(0,100,(500,))
+#y_data = np.random.randint(0,100,(500,))
+#z_data = np.random.randint(0,100,(500,))
+#ax.scatter(x_data,y_data,z_data,marker="v",alpha=0.1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+x_data = np.arange(-5,5,0.1)
+y_data = np.arange(-5,5,0.1)
 
+X,Y = np.meshgrid(x_data,y_data)
+Z = np.sin(X) * np.cos(Y)
 
-# Press the green button in the gutter to run the script.
+ax.plot_surface(X,Y,Z,cmap="plasma")
+ax.view_init(azim=0,elev=90)
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

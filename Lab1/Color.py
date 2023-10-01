19f8bc35b1b364280b0bbfc6da1ecca6705b206e
Abdyukov Zufar <swat55551@gmail.com>
2023-10-01 12:11:18 +0300

    Refactor Add normal front (combo box, tabs) Tab #1 gradient

---
+++ Lab1/Color.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+    CYAN = "cyan"
+    YELLOW = "yellow"
+    PINK = "pink"
+    BLACK = "black"
+    WHITE = "white"
+    GRAY = "gray"
+class ColorFigure(Enum):
+    PLASMA = "plasma"
+    VIRIDIS = "viridis"
+    INFERNO = "inferno"
+    MAGMA = "magma"
+    CIVIDIS = "cividis"
+    BINARY = "binary"
+    SPRINT = "sprint"
+    SUMMER = "summer"
+    WINTER = "winter"
+    AUTUMN = "autumn"
+    BONE = "bone"
+
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,7 +1,4 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-from tkinter import filedialog
+from imports_tkinter import *
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -10,63 +7,22 @@ from mpl_toolkits import mplot3d  # for 3D
 from matplotlib.animation import FuncAnimation
 
 import numpy as np
-from enum import Enum
 import random
+
 from gradient_descent import gradient_descent
+from Color import *
 from setFunction import *
 from frontshow.placement_elements import createLabel
-# ax.scatter(x_data,y_data,z_data,marker="v",alpha=0.1)
-
-#
-# X,Y = np.meshgrid(x_data,y_data)
-# Z = np.sin(X) * np.cos(Y)
-#
-# ax.plot_surface(X,Y,Z,cmap="plasma")
-# ax.view_init(azim=0,elev=90)
+from global_variable import *
 
 fig_3d = plt.figure()
 ax_3d = fig_3d.add_subplot(111, projection="3d")
-START = -5
-END = 5
-STEP = 0.6
-SPEED = 100
-SIZE_POINT = 50
-
-
-class Color(Enum):
-    RED = "red"
-    GREEN = "green"
-    BLUE = "blue"
-    CYAN = "cyan"
-    YELLOW = "yellow"
-    PINK = "pink"
-    BLACK = "black"
-    WHITE = "white"
-    GRAY = "gray"
-class ColorFigure(Enum):
-    PLASMA = "plasma"
-    VIRIDIS = "viridis"
-    INFERNO = "inferno"
-    MAGMA = "magma"
-    CIVIDIS = "cividis"
-    BINARY = "binary"
-    SPRINT = "sprint"
-    SUMMER = "summer"
-    WINTER = "winter"
-    AUTUMN = "autumn"
-    BONE = "bone"
-
-
-x_data = []
-y_data = []
-
-scatter_points = []
 
 def fillMatrix(resultGradient):
     temp = []
+    print(resultGradient)
     for x, y, countIter, f in resultGradient:
-        if(countIter % 2 == 0):
-            temp.append([x, y, f - 5]) # -5 for better show
+        temp.append([x, y, f])
     return temp
 
 def animate(frame, arr, arr2):
@@ -75,97 +31,25 @@ def animate(frame, arr, arr2):
     if len(arr2) > frame:
         scatter_points.append(ax_3d.scatter(arr2[frame][0], arr2[frame][1], arr2[frame][2], c=Color.RED.value, marker='o', s=SIZE_POINT))
 
-def callGradient_DrawPoint() -> None:
+def callGradient_DrawPoint(textFieldCountIter) -> None:
     try:
-        if(len(x_data) > 0 and len(y_data) > 0):
+        if(len(x_data) > 0 and len(y_data) > 0 and int(textFieldCountIter.get()) > 10):
             # Moment draw 2 min
-            temp = fillMatrix(list(gradient_descent(function_1, random.choice(x_data), random.choice(y_data), 0.1, 500)))
-            temp2 = fillMatrix(list(gradient_descent(function_1, random.choice(x_data), random.choice(y_data), 0.1, 500)))
+            temp = fillMatrix(list(gradient_descent(current_function, random.choice(x_data), random.choice(y_data), 0.1, int(textFieldCountIter.get()))))
+            temp2 = fillMatrix(list(gradient_descent(current_function, random.choice(x_data), random.choice(y_data), 0.1, int(textFieldCountIter.get()))))
 
             ani = FuncAnimation(fig_3d, animate, frames=max(len(temp), len(temp2)), fargs=(temp, temp2,), interval=SPEED,
                             repeat=False)
             canvas_3d.draw()
         else:
-            messagebox.showerror("Error", "Invalid data. Fill defualt data.")
+            print(len(x_data))
+            print(len(y_data))
+            print(int(textFieldCountIter.get()))
+            messagebox.showerror("Error", "Invalid data. Fill defualt data and count iter.")
     except ValueError:
         messagebox.showerror("Error", "Invalid fillMatrix. Please check fill matrix and gradient_descent")
 
-def setBaseData():
-    try:
-        global START
-        global END
-        global STEP
-        global x_data
-        global y_data
-        if len(entry_end.get()) > 0 and len(entry_start.get()) and len(entry_step.get()):
-            START = float(entry_start.get())
-            END = float(entry_end.get())
-            STEP = float(entry_step.get())
-            x_data = np.arange(START, END, STEP)
-            y_data = np.arange(START, END, STEP)
-        else:
-            messagebox.showerror("Error", "Invalid input. Not enought values")
-    except ValueError:
-        messagebox.showerror("Error", "Invalid input. Please enter numeric values.")
 
-'''
-def update_plot() -> None:
-    try:
-        global temp_input_x
-        global temp_input_y
-        # x = float(entry_x.get())
-        # y = float(entry_y.get())
-        input_x = entry_x.get()
-        input_y = entry_y.get()
-        points = [float(point) for point in input_x.split()]
-        if True == True:  # checkPointSimple(input_x,input_y)== True:
-            temp_input_x = input_x
-            temp_input_y = input_y
-            # entry_x.delete(0,'end')
-            # entry_y.delete(0, 'end')
-
-            point_x = [float(point) for point in input_x.split()]
-            point_y = [float(point) for point in input_y.split()]
-          
-            if (len(point_x) <2 or len(point_y) < 2) and len(point_x) != len(point_y):
-                messagebox.showerror("Error","Please enter at least two numberic")
-                return
-    
-            for i in point_x:
-                data_points['x'].append(i)
-            for i in point_y:
-                data_points['y'].append(i)
-            for i in range(len(data_points['x'])):
-                data_points['z'].append(data_points['x'][i] + data_points['y'][i])
-  
-            for i in range(0, len(points), 2):
-                x, y = points[i:i + 2]
-                data_points['x'].append(x)
-                data_points['y'].append(y)
-                data_points['z'].append(x + y)
-            # Clear the previous 3D plot
-            ax_3d.clear()
-
-            ax_3d.scatter(data_points['x'], data_points['y'], data_points['z'], c='r', marker='o')
-
-            # Example 3D plot (replace with your own data)
-            # ax_3d.scatter(x, y, x + y, c='r', marker='o')
-
-            # Customize the 3D plot
-            ax_3d.set_title("3D Matplotlib Plot")
-            ax_3d.set_xlabel("X-axis")
-            ax_3d.set_ylabel("Y-axis")
-            ax_3d.set_zlabel("Z-axis")
-
-            # Draw the updated 3D plot on the canvas
-            canvas_3d.draw()
-        else:
-            messagebox.showerror("Error", "This point already added")
-
-    except ValueError:
-        messagebox.showerror("Error", "Invalid input. Please enter numeric values.")
-
-'''
 # Create a function to add a new point to the list
 def save_plot():
     try:
@@ -181,7 +65,7 @@ def save_plot():
 def close_application():
     root.quit()
 
-def buildBaseFunction(ax_3d,function):
+def buildBaseFunction():
     ax_3d.clear()
 
     ax_3d.set_title("3D Matplotlib Plot")
@@ -189,16 +73,20 @@ def buildBaseFunction(ax_3d,function):
     ax_3d.set_ylabel("Y-axis")
     ax_3d.set_zlabel("Z-axis")
 
+    global x_data
+    global y_data
+    if type(START) == dict:
+        x_data = np.arange(START[1], START[2], STEP)
+        y_data = np.arange(END[1], END[2], STEP)
+    else:
+        x_data = np.arange(START, END, STEP)
+        y_data = np.arange(START, END, STEP)
+
     X, Y = np.meshgrid(x_data, y_data)
-    Z = function(X, Y)
+    Z = current_function(X, Y)
 
     ax_3d.plot_surface(X, Y, Z, cmap=ColorFigure.BONE.value)
     canvas_3d.draw()
-
-def fillEntryDefaultForTest(arr_entry):
-    arr_entry[0].insert(0,"-5")
-    arr_entry[1].insert(0, "5")
-    arr_entry[2].insert(0, "0.8")
 
 def clearPoint():
     for i in scatter_points:
@@ -206,48 +94,85 @@ def clearPoint():
     canvas_3d.draw()
     scatter_points.clear()
 
+def selectFunc(event,combo,lab,textField):
+    try:
+        global STEP
+        if len(textField.get()) > 0 and textField.get()!='' and float(textField.get())!=0:
+            selection = combo.get()
+            STEP = float(textField.get())
+            #print(selection)
+            global current_function
+            global START
+            global END
+            if selection in chooseFunc:
+                current_function = chooseFunc[selection]['f']
+            if selection == "Bukina":
+                START = chooseFunc[selection]['x']
+                END = chooseFunc[selection]['y']
+            else:
+                START = chooseFunc[selection]['from']
+                END = chooseFunc[selection]['to']
+            buildBaseFunction()
+            lab.config(text="Function:" + selection)
+        else:
+            messagebox.showerror("Error", "Invalid input. Please enter numeric values step.")
+    except ValueError:
+        messagebox.showerror("Error", "Invalid input. Please enter numeric values.")
+
+
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Matplotlib with Windows Form 3D")
 
+    notebook = ttk.Notebook(root)
+
     canvas_3d = FigureCanvasTkAgg(fig_3d, master=root)
     canvas_3d_widget = canvas_3d.get_tk_widget()
 
-    createLabel.placement_label(root,"Start",0,1,5,1,1,5)
-    createLabel.placement_label(root, "END", 1, 1, 5, 1, 1, 5)
-    createLabel.placement_label(root, "STEP", 2, 1, 5, 1, 1, 5)
+    frame1 = ttk.Frame(notebook)
+    frame2 = ttk.Frame(notebook)
 
-    entry_start = ttk.Entry(root)
-    entry_end = ttk.Entry(root)
-    entry_step = ttk.Entry(root)
+    notebook.add(frame1, text="Tab1")
+    notebook.add(frame2,text="Tab2")
 
-    fillEntryDefaultForTest([entry_start, entry_end, entry_step])
+    textFieldStep = ttk.Entry(frame1)
+    textFieldStep.insert(0, "0.1")
 
-    findMinGradientDescent_button = ttk.Button(root, text="Call gradient Descent", command=lambda: callGradient_DrawPoint())
-    buildBaseFunc_btn = ttk.Button(root, text="Build base Plot",
-                                   command=lambda: buildBaseFunction(ax_3d, function_1))
-    clear_btn = ttk.Button(root,text="Clear points",command=clearPoint)
-    addDefaultValue_btn = ttk.Button(root, text="Set point", command=setBaseData)
-    save_button = ttk.Button(root, text="Save Schedule", command=save_plot)
-    close_button = ttk.Button(root, text="Close Application", command=close_application)
+    lab_func = createLabel.placement_label(frame1, "Choose function", 0, 1, 5, 1, 3, 5)
+
+    comboBoxFunc = ttk.Combobox(frame1, values=["function_1", "function_2", "Bila", "Buta", "Bukina", "Eggholder"],
+                                state="readonly")
+    comboBoxFunc.bind("<<ComboboxSelected>>", lambda event, cb=comboBoxFunc,lab = lab_func,field=textFieldStep: selectFunc(event, cb,lab,field))
+
+    createLabel.placement_label(frame1, "Choose Step", 2, 1, 5, 1, 3, 5)
+
+    createLabel.placement_label(frame1, "Choose count iteration for gradient", 4, 1, 5, 1, 3, 5)
+    textFieldCountIter = ttk.Entry(frame1)
+    textFieldCountIter.insert(0, "500")
+
+    findMinGradientDescent_button = ttk.Button(frame1, text="Call gradient Descent", command=lambda: callGradient_DrawPoint(textFieldCountIter))
+
 
     canvas_3d_widget.grid(row=0, column=0, rowspan=20, padx=5, pady=5) #matplotlib
+    notebook.grid(row=0,column=1,padx=5,pady=5,rowspan=1,columnspan=3,sticky="nsew")
 
-    entry_start.grid(row=0, column=2,padx=5, pady=5,rowspan=1,columnspan=1,sticky="nsew")
-    entry_end.grid(row=1, column=2,padx=5, pady=5,rowspan=1,columnspan=1,sticky="nsew")
-    entry_step.grid(row=2, column=2,padx=5, pady=5,rowspan=1,columnspan=1,sticky="nsew")
+    comboBoxFunc.grid(row=1,column=1,padx=5,pady=5,rowspan=1,columnspan=3,sticky="nsew")
+    textFieldStep.grid(row=3, column=1,padx=5, pady=5,rowspan=1,columnspan=3,sticky="nsew")
+    textFieldCountIter.grid(row=5, column=1, padx=5, pady=5, rowspan=1, columnspan=3, sticky="nsew")
+    findMinGradientDescent_button.grid(row=6, column=1, columnspan=3, padx=5, pady=5, sticky="nsew")
 
+    clear_btn = ttk.Button(frame1, text="Clear points", command=clearPoint)
+    save_button = ttk.Button(root, text="Save Schedule", command=save_plot)
 
-    addDefaultValue_btn.grid(row=3,column=1,columnspan=2, padx=5, pady=5, sticky="nsew")
-    buildBaseFunc_btn.grid(row=4, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
-    findMinGradientDescent_button.grid(row=5, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
-    clear_btn.grid(row=6,column=1,columnspan=2, padx=5, pady=5, sticky="nsew")
-    save_button.grid(row=7, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
-    close_button.grid(row=8, column=1, columnspan=2, padx=5, pady=5, sticky="nsew")
+    clear_btn.grid(row=7,column=1,columnspan=3, padx=5, pady=5, sticky="nsew")
+    save_button.grid(row=8, column=1, columnspan=3, padx=5, pady=5, sticky="nsew")
+    close_button = ttk.Button(root, text="Close Application", command=close_application)
+    close_button.grid(row=9, column=1, columnspan=3, padx=5, pady=5, sticky="nsew")
 
     # Configure row and column weights for resizing
     root.grid_rowconfigure(0, weight=1)
-    root.grid_rowconfigure(9, weight=1)
+    root.grid_rowconfigure(10, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(3, weight=1)
 

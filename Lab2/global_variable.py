3f8e74413b645dd714bac812d9f1980ce0c8398c
Abdyukov Zufar <swat55551@gmail.com>
2023-10-14 10:44:40 +0300

    Solution lab2 Add textReach for show value's points Realize tab2 Create tab3 Add new function quadratic

---
+++ Lab2/global_variable.py
@@ -0,0 +1,18 @@
+START = -5
+END = 5
+STEP = 0.5
+
+SPEED = 100
+SIZE_POINT = 50
+
+current_function = None
+x_data = []
+y_data = []
+
+scatter_points = []
+
+canvas_3d = None
+canvas_3d_widget = None
+
+textReachGradientPoint = None
+textReachQuadPoint = None
